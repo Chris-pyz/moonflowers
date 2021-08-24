012a49f066ee96a3a718add505feaaffcd15d4e3
Christophe JULLIEN <84345980+Chris-pyz@users.noreply.github.com>
2021-08-24 15:23:34 +0200

    add photo avatar with cloudinary

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -49,6 +49,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:nickname, :name, :self_description)
+    params.require(:user).permit(:nickname, :name, :self_description, :avatar_photo)
   end
 end
