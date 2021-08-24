77f823947b1ec0290d78ff32dee32c6d90e754ef
Genevieve Michaud <migenevieve@gmail.com>
2021-08-24 17:41:51 +0200

    MapBoxsansStyleActivityIndex

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,2 +1,14 @@
 class ActivitiesController < ApplicationController
+
+  def index
+
+    @activities = Activity.all
+    # the `geocoded` scope filters only flats with coordinates (latitude & longitude)
+    @markers = @activities.geocoded.map do |activity|
+      {
+        lat: activity.latitude,
+        lng: activity.longitude
+      }
+    end
+  end
 end
