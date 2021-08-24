d79b1b7f72e351478717acdedf267173e776b9cc
ArnaudPriet <arnaud.priet1@gmail.com>
2021-08-24 15:51:14 +0200

    change timestemp from user migration(23 vers 22)

--- db/schema.rb
+++ db/schema.rb
@@ -15,15 +15,6 @@ ActiveRecord::Schema.define(version: 2021_08_24_130533) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "activities", force: :cascade do |t|
-    t.float "latitude"
-    t.float "longitude"
-    t.bigint "user_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["user_id"], name: "index_activities_on_user_id"
-  end
-
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -43,6 +34,15 @@ ActiveRecord::Schema.define(version: 2021_08_24_130533) do
     t.string "checksum", null: false
     t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "activities", force: :cascade do |t|
+    t.float "latitude"
+    t.float "longitude"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_activities_on_user_id"
   end
 
   create_table "likes", force: :cascade do |t|
@@ -79,12 +79,9 @@ ActiveRecord::Schema.define(version: 2021_08_24_130533) do
     t.index ["activity_id"], name: "index_wastes_on_activity_id"
   end
 
-
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "activities", "users"
   add_foreign_key "likes", "activities"
-
-  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
-
   add_foreign_key "likes", "users"
   add_foreign_key "wastes", "activities"
 end
