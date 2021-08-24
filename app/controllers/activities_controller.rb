f05f0f02d01cdc69fb64639b01cf1686eb7b1923
anais meirone <anais.meirone@live.fr>
2021-08-24 16:31:13 +0200

    add show div

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,2 +1,58 @@
 class ActivitiesController < ApplicationController
+
+  # get /activities -actions_path
+  def index
+    @activities = Activity.all
+  end
+
+  # get /activities/:id -action_path
+  def show
+    @activity = Activity.find(params[:id])
+  end
+
+  # get /activities/new -new_action_path
+  def new
+    @activity = Activity.new
+  end
+
+  # post /activities -action_path
+  def create
+    @activity = Activity.new(activities_params)
+    @activity.user = current_user
+
+    if @activity.save
+      redirect_to @activity, notice: 'Your activity was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  # get /activities/:id/edit -edit_action_path
+  def edit
+    @activity = Activity.find(params[:id])
+  end
+
+  # patch(post?) /activities/:id -action_path
+  def update
+    @activity = Activity.find(params[:id])
+    if @activity.update(action_params)
+      redirect_to @activity, notice: 'Your activity was successfully update.'
+    else
+      render :edit
+    end
+  end
+
+  # delete /activities/:id -action_path
+  # def destroy
+  #   @activity = Activity.find(params[:id])
+  #   @activity.destroy
+  #   redirect_to actions_path, notice: 'Your activity was successfully destroyed.'
+  # end
+
+  private
+
+  def activities_params
+    params.require(:activity).permit(:longitude, :latitude)
+  end
+
 end
