b7bf64d7eb5272c95782e392434da22369ac1e3c
Christophe JULLIEN <84345980+Chris-pyz@users.noreply.github.com>
2021-08-24 17:47:53 +0200

    first method for likecontroller

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,2 +1,17 @@
 class LikesController < ApplicationController
+
+
+  def show
+    # @user = current_user
+    @like = Like.find(params[:id])
+  end
+
+    def edit
+    @like = current_user
+  end
+
+  # /users/:id
+  def update
+    @like = current_user
+  end
 end
