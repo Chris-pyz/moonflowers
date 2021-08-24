2620ea2ccff0f4b2b0bab6fbfd73b87409ee7bba
Genevieve Michaud <migenevieve@gmail.com>
2021-08-24 11:47:14 +0200

    ViewUsers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,39 @@
 class UsersController < ApplicationController
 
+  # /users
   def index
     @users = User.all
   end
 
+  # users/:id
   def show
     @user = current_user
   end
+
+  # users/new
+  def new
+  end
+
+  # /users
+  def create
+  end
+
+  # /users/:id/edit
+  def edit
+  end
+
+  # /users/:id
+  def update
+  end
+
+  # /users/:id
+  def destroy
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:nickname, :name, :email, :password, :self_description)
+  end
+
 end
