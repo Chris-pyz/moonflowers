bf0526d8e5da76e95f7c6de1b42957b007c07d24
Genevieve Michaud <migenevieve@gmail.com>
2021-08-25 10:54:31 +0200

    debutinfoWindow

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -8,7 +8,9 @@ class ActivitiesController < ApplicationController
       {
         lat: activity.latitude,
         lng: activity.longitude
+        info_window: render_to_string(partial: "info_window", locals: { activity: activity })
       }
+
     end
   end
 end
