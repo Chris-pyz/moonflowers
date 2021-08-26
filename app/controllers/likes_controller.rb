36f7baaca8714bd89b8175e822ea30f6a15d59ed
Christophe JULLIEN <84345980+Chris-pyz@users.noreply.github.com>
2021-08-26 14:28:20 +0200

    like incrémentation ok sur page activities show

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,14 +2,21 @@ class LikesController < ApplicationController
   # /users/:id
   def create
     # creation de l'instance like
-    @like = Like.new
-    # assignation de @activity avec l'action du user actuel
     @activity = Activity.find(params[:activity_id])
-    # le like est le like du user actuel
-    @like.user = current_user
-    # assignation du like sur l'action actuelle
-    @like.activity = @activity
-    @like.save
-    redirect_to @activity
+
+    if (@activity.likes.select { |l| l.user == current_user }).count.zero?
+
+      @like = Like.new
+      # assignation de @activity avec l'action du user actuel
+
+      # le like est le like du user actuel
+      @like.user = current_user
+      # assignation du like sur l'action actuelle
+      @like.activity = @activity
+      @like.save
+      redirect_to @activity
+    else
+      "Déjà liké"
+    end
   end
 end
