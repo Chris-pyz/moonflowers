6e6dcb4759d53cd0fb90b3bd39f9965b9e1bd017
Christophe JULLIEN <84345980+Chris-pyz@users.noreply.github.com>
2021-09-01 11:30:57 +0200

    started fixed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,21 +10,22 @@ class UsersController < ApplicationController
   def show
     # @user = current_user
     @user = User.find(params[:id])
+    @user_wastes = @user.waste_quantities
 
-    @wastes = Waste.all
-    @allglass = @user.waste_quantities["Verre"]
-    @allmetals = @user.waste_quantities["Métal"]
-    @allPlastics = @user.waste_quantities["Plastique"]
-    @allpapers = @user.waste_quantities["Papier"]
-    @alldivers = @user.waste_quantities["Divers"]
+    # @wastes = Waste.all
+    # @allglass = @user.waste_quantities["Verre"]
+    # @allmetals = @user.waste_quantities["Métal"]
+    # @allPlastics = @user.waste_quantities["Plastique"]
+    # @allpapers = @user.waste_quantities["Papier"]
+    # @alldivers = @user.waste_quantities["Divers"]
 
     @totalwasteall = @allglass + @allmetals + @allPlastics + @allpapers + @alldivers
 
-    @allglassforchart = @allglass.to_f / @totalwasteall.to_f
-    @allmetalsforchart = @allmetals.to_f / @totalwasteall.to_f
-    @allplasticsforchart = @allPlastics.to_f / @totalwasteall.to_f
-    @allpapersforchart = @allpapers.to_f / @totalwasteall.to_f
-    @alldiversforchart = @allPlastics.to_f / @totalwasteall.to_f
+    @allglassforchart = @allglass.to_f / @totalwasteall
+    @allmetalsforchart = @allmetals.to_f / @totalwasteall
+    @allplasticsforchart = @allPlastics.to_f / @totalwasteall
+    @allpapersforchart = @allpapers.to_f / @totalwasteall
+    @alldiversforchart = @allPlastics.to_f / @totalwasteall
 
     @totalforchart = @allglassforchart + @allmetalsforchart + @allplasticsforchart + @allpapersforchart + @alldiversforchart
   end
